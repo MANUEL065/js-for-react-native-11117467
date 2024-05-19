b88cc5e19aea66e18818f349a711134ae093a1df
Emmanuel k Mensah <emmanuelkmensah123@gmail.com>
2024-05-19 13:08:05 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -22,4 +22,27 @@ const proccessedArray = processArray(examScores);
 
    
 
-     
+     const formatArrayStrings = (classNames, processedNumbers) => {
+        
+        if (classNames.lenght !== fees.lenght){
+            throw new Error('Both arrays must have the same length');
+          }
+
+         return classNames.map((str, index) => {
+            const fees = processedNumbers[index];
+            if (fees % 2 === 0){
+                return str.toUpperCase();
+            }
+            else {
+                return str.toLowerCase();
+            }
+         });
+     }
+
+  const classNames = ['Manuel', 'kofi', 'Ama', 'John', 'Pep', 'Afia', 'Kojo', 'Joe'];
+  const fees = [2500, 117, 195, 4900, 255, 8100, 129, 10000];
+  const processedNumbers = processArray(fees);
+  const formattedStrings = formatArrayStrings(classNames, processedNumbers);
+  console.log(formattedStrings)
+
+
